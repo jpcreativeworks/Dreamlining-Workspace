6433a20ca01537626f070afc1bdf47701858c2c8
jpcreativeworks <jp.graphics.011@gmail.com>
2022-05-13 17:53:30 -0700

    worked on making the client login and backend routes

---
+++ node-react-app/client/src/components/login/Login.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { validateEmail, checkPassword } from '../../utils/helpers';
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleInputUpdate = (e) => {
+        // Getting the value and name of the input which triggered the change
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+    
+        
+        if (inputType === 'emailAddress') {
+          setEmail(inputValue);
+  
+        } else {
+          setPassword(inputValue)
+        }
+      };
+    
+      const handleLoginSubmit = (e) => {
+        e.preventDefault();
+    
+        if (!validateEmail(email)) {
+          setErrorMessage('Email is invalid');
+          return;
+        }
+        if (!checkPassword(password)) {
+          setErrorMessage(
+            `Choose a more secure password for the account: ${email}`
+          );
+          return;
+        }
+    
+        setPassword('')
+        setEmail('');
+        setErrorMessage('');
+        alert(`Hello ${email}`);
+      };
+    return (
+        <div> 
+            <p>Hello {email}</p>
+            <form className="form">
+                <label>Email</label>
+                <input value={email}
+                name='emailAddress'
+                onChange={handleInputUpdate}
+                type="text"
+                placeholder="email Address" 
+                />
+                <label>Password</label>
+                <input value={password}
+                name="password"
+                type="password"
+                onChange={handleInputUpdate}
+                placeholder="password" 
+                />
+                <button type="button" onClick={handleLoginSubmit}>Login</button>
+            </form>
+            {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+        </div>
+    );
+}
+
+export default Login;
